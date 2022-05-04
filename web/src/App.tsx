c316be330cbba01da61be4826fa740d30d0b5dcc
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-04 03:20:43 -0300

    feat: Hello Word para nao dar azar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,22 @@
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button 
+  className="bg-red-400 rounded px-4 h-10 hover:bg-red-600 transition-colors ">
+  {props.text ?? 'Default'}
+  </button>
+}
+
 function App() {
-  return <h1>Hello word</h1>
+  return (
+    <div className="flex gap-3">
+      <Button text="Registrar"/>
+      <Button text="Entrar"/>
+    <Button />
+    </div>
+  )
 }
 
 export default App

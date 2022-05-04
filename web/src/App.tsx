1591765ecc96b6e2d51ba57964509ddef321bf51
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-04 03:22:42 -0300

    refactor: Removendo codigos testes do App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
-
-function Button(props: ButtonProps) {
-  return <button 
-  className="bg-red-400 rounded px-4 h-10 hover:bg-red-600 transition-colors ">
-  {props.text ?? 'Default'}
-  </button>
-}
-
 function App() {
-  return (
-    <div className="flex gap-3">
-      <Button text="Registrar"/>
-      <Button text="Entrar"/>
-    <Button />
-    </div>
-  )
+  return 
 }
 
 export default App

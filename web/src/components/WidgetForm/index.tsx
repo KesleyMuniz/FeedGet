4ffdab38e3698c60e84a18f82ae1a2e91cd4d171
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-05 03:12:46 -0300

    feat: Importando o FeedbackContentStep

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-import { CloseButton } from "../CloseButton";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
 import bugImageURL from '../../images/bug.svg';
 import ideaImageURL from '../../images/idea.svg';
 import thoughtImageURL from '../../images/thought.svg';
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -35,14 +35,22 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-      ) : 
-      <p>Tipo de feedback já selecionado </p>
-      }
+      ) : (
+        <FeedbackContentStep 
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
       
       <footer className="text-xs text-neutral text-neutral-400">
       Feito com ♥ pela <a className="underline underline-offset-2">Rocketseat</a> 

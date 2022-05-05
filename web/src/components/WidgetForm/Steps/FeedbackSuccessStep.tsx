c78088678b20395e5d26c3ba0c0aa1df4957acd9
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-05 04:51:38 -0300

    feat: Criada logica para o usuario enviar outro feedback pelo botao

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,6 +1,12 @@
 import { CloseButton } from "../../CloseButton";
 
-export function FeedbackSuccessStep() {
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({
+  onFeedbackRestartRequested
+}: FeedbackSuccessStepProps) {
   return (
     <>
     <header>
@@ -15,7 +21,11 @@ export function FeedbackSuccessStep() {
 
     <span className="text-xl mt-2">Agradecemos o Feedback</span>
 
-    <button className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500">
+    <button 
+      className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
+      type="button"
+      onClick={onFeedbackRestartRequested}
+      >
       Quero enviar outros!
     </button>
     </div>

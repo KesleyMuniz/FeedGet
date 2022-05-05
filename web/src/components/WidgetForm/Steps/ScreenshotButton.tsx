f7e540e3069bfbffc9f20ef8aa57a27623ec743d
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-05 03:42:43 -0300

    feat: criado o estado para exibir o icone de carregamento

--- web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/Steps/ScreenshotButton.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 import { Camera } from "phosphor-react";
 import html2canvas from "html2canvas";
 
 export function ScreenshotButton() {
+  const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
+
 async function handleTakeScreenshot() {
   const canvas = await html2canvas(document.querySelector('html')!)
   const base64image = canvas.toDataURL('image/png')

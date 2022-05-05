720e914581bf53ef8c395708317cc3a9eef3b7bf
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-05 03:48:18 -0300

    feat: Criando e importando o icone de loading

--- web/src/components/WidgetForm/ScreenshotButton.tsx
+++ web/src/components/WidgetForm/ScreenshotButton.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { Camera } from "phosphor-react";
 import html2canvas from "html2canvas";
+import Loading from "./Loading";
 
 export function ScreenshotButton() {
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
@@ -17,7 +18,7 @@ async function handleTakeScreenshot() {
     className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
     onClick={handleTakeScreenshot}
     >
-      <Camera className="w-6 h-6"/>
+      { isTakingScreenshot ? <Loading />: <Camera className="w-6 h-6"/>}
     </button>
   )
 }

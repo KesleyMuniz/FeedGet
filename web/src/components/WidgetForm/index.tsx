fdc78469411215d56052edc8ac2f5c5daa405702
KesleyMuniz <contato.kgmstudios@hotmail.com>
2022-05-05 02:25:54 -0300

    feat: importando o CloseButton para dentro do componente TypeStep

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -37,15 +37,12 @@ export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
-
-        <CloseButton />
-      </header>
-
+      
       {!feedbackType ? (
         <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-      ) : <p>Tipo de feedback já selecionado </p>}
+      ) : 
+      <p>Tipo de feedback já selecionado </p>
+      }
       
       <footer className="text-xs text-neutral text-neutral-400">
       Feito com ♥ pela <a className="underline underline-offset-2">Rocketseat</a> 
